37546cfcb9c4fd8686dbe06d8e9bfd95ec183768
Gayathri Srinivasan <gssrinivasan@users.noreply.github.com>
2015-10-08 15:03:16 -0400

    Update myc.h

--- myC/myc.h
+++ myC/myc.h
@@ -5,9 +5,21 @@
 #include <list>
 
 using namespace std;
-class exp_node {
+
+class statement {
+ public:
+  virtual void print() {}
+  //virtual void evaluate() = 0;
+};
+
+class exp_node: public statement{
   public:
+
+    // print function for pretty printing an expression
     virtual void print() = 0;
+
+    // evaluation function for a leaf, replaced for interior nodes
+    //virtual float evaluate() = 0;
 };
 
 class operator_node : public exp_node {
@@ -16,159 +28,233 @@ public:
     exp_node *right;
     operator_node(exp_node *L, exp_node *R);
 };
-class number_node : public exp_node {
- private:
-    float num;
 
- public:
-  number_node(float value);
-  void print();
-};
-
-class id_node : public exp_node {
+class arr1d_var : public exp_node {
 protected:
-  string id, var_type;
-
+string id;
+float value;
 public:
-  id_node(String var_type, string value);
-  void print();
+arr1d_var( string id, float value);
+void print();
 };
 
-class function_or_var_list: public exp_node {
- protected:
-  statement *function, exp_node *function_or_var_list, statement *global_var;
- public:
-  func_node(exp_node *function_or_var_list, statement *function);
-  void print();
-  func_node1(exp_node *function_or_var_list, statement *global_var);
-  void print();
+class arr2d_var : public exp_node {
+protected:
+string id;
+float value1;
+float value2;
+public:
+arr2d_var(string id, float value1,float value2);
+void print();
 };
 
-class function: public statement, id_node, arg_node {
- protected:
- string var_type, id;
- exp_node *arg_node;
- public:
- id_node (var_type, id);
- void print();
- arg_node	(arg_node); //need to be defined
- void print();
+class assignment_stmt: public statement{
+protected:
+string id;
+exp_node *expr;
+public:
+assignment_stmt (string id, exp_node *expr);
+void print();
 };
 
-/*
-
-class number_node : public exp_node {
- private:
-    float num;
-
- public:
-  number_node(float value);
-  void print();
-  //float evaluate();
+class logical_oror: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_oror(exp_node *expr1, exp_node *expr2);
+void print();
 };
 
-class unary_minus_node : public exp_node {
- protected:
-  exp_node *exp;
- public:
-  unary_minus_node(exp_node *exp);
-  void print();
-  //float evaluate();
+class logical_andand: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_andand(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class logical_equalequal: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_equalequal(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class logical_notequal: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_notequal(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class logical_less: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_less(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class logical_great: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_great(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class logical_lessequal: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_lessequal(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class logical_greatequal: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+logical_greatequal(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class plus_expression: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+plus_expression(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class minus_expression: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+minus_expression(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class times_expression: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+times_expression(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class divide_expression: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+divide_expression(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class modulo_expression: public exp_node{
+protected:
+exp_node *expr1;
+exp_node *expr2;
+public:
+modulo_expression(exp_node *expr1, exp_node *expr2);
+void print();
+};
+
+class if_then_stmt: public statement{
+protected:
+exp_node *expr;
+statement *st;
+public:
+if_then_stmt(exp_node *expr, statement *st);
+void print();
+};
+
+class if_then_else_stmt: public statement{
+protected:
+exp_node *expr;
+statement *st1;
+statement *st2;
+public:
+if_then_else_stmt(exp_node *expr, statement *st1, statement *st2);
+void print();
+};
+
+
+class while_stmt: public statement{
+protected:
+exp_node *expr;
+statement *st;
+public:
+while_stmt(exp_node *expr, statement *st);
+void print();
+};
+
+
+class do_while_stmt: public statement{
+protected:
+exp_node *expr;
+statement *st;
+public:
+do_while_stmt(statement *st, exp_node *expr);
+void print();
+};
+
+
+class for_stmt: public statement{
+protected:
+statement *initializaton;
+exp_node *condition;
+statement *incrementation;
+statement *st;
+public:
+for_stmt(statement *initialization, exp_node *condition, statement *incrementation, statement *st);
+void print();
 };
 
 class id_node : public exp_node {
 protected:
   string id;
-
 public:
   id_node(string value);
   void print();
-  //float evaluate();
 };
 
-// plus_node inherits the characteristics of node and adds its own evaluate function
-class plus_node : public operator_node {
-  public:
-
-  // plus_node's constructor just uses node's constructor
-  plus_node(exp_node *L, exp_node *R);
-  void print();
-  //float evaluate();
-};
-
-
-// minus_node inherits the characteristics of node and adds its own evaluate function
-class minus_node : public operator_node {
-  public:
-
-  minus_node(exp_node *L, exp_node *R);
-  void print();
-  //float evaluate();
-};
-
-
-// times_node inherits the characteristics of node and adds its own evaluate function
-class times_node : public operator_node {
-  public:
-
-  times_node(exp_node *L, exp_node *R);
-  void print();
-  //float evaluate();
-};
-
-
-// divide_node inherits the characteristics of node and adds its own evaluate function
-class divide_node : public operator_node {
-  public:
-
-  divide_node(exp_node *L, exp_node *R);
-  void print();
-  //float evaluate();
-};
-
-class statement {
+class number_node : public exp_node {
+ private:
+    float num;
  public:
-  virtual void print() {}
-  //virtual void evaluate() = 0;
+  number_node(float value);
+  void print();
 };
 
-class assignment_stmt : public statement {
- protected:
+class var_node : public exp_node {
+protected:
   string id;
-  exp_node *exp;
- public:
-  assignment_stmt(string name, exp_node *expression);
+  exp_node *list;
+  
+public:
+  var_node(string value, exp_node *list );
   void print();
-  //void evaluate();
 };
 
-class print_stmt: public statement {
- protected:
-  exp_node *exp;
- public:
-  print_stmt(exp_node *myexp);
-  void print();
-  //void evaluate();
-};
-
-class skip_stmt: public statement {
+class skip_stmt: public exp_node {
  public:
   skip_stmt();
   void print();
   //void evaluate();
 };
-
-
-class sequence_stmt: public statement {
- protected:
-  statement *stmt1, *stmt2;
- public:
-  sequence_stmt(statement *mystmt1, statement *mystmt2);
-  void print();
-  //void evaluate();
-};
-
-*/
-// the object at the base of our tree
 extern map<string, float> idTable;
-
